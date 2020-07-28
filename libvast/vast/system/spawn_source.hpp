9845ca3353bc61140012304edfebfc2bf4a5a9a1
Tobias Mayer <tobim@fastmail.fm>
2020-07-28 14:31:05 +0200

    Revert "Merge pull request #964"
    This reverts commit 4d1a2d64e1b0baa2dd0c5e6abaa51aec76bfb580, reversing
    changes made to bd797f77d64f6a0f24f837ddee645ba6e94f93ca.

--- libvast/vast/system/spawn_source.hpp
+++ libvast/vast/system/spawn_source.hpp
@@ -13,48 +13,52 @@
 
 #pragma once
 
+#include "vast/aliases.hpp"
 #include "vast/fwd.hpp"
-#include "vast/logger.hpp"
-#include "vast/system/make_source.hpp"
-#include "vast/system/node.hpp"
-#include "vast/system/spawn_arguments.hpp"
 
 namespace vast::system {
 
-/// Tries to spawn a new SOURCE for the specified format.
-/// @tparam Reader the format-specific reader
+/// Tries to spawn a new SOURCE for the CSV format.
 /// @param self Points to the parent actor.
 /// @param args Configures the new actor.
 /// @returns a handle to the spawned actor on success, an error otherwise
-template <class Reader, class Defaults = typename Reader::defaults>
-maybe_actor spawn_source(node_actor* self, spawn_arguments& args) {
-  VAST_TRACE(VAST_ARG("node", self), VAST_ARG(args));
-  auto& st = self->state;
-  auto& options = args.inv.options;
-  // Bail out early for bogus invocations.
-  if (caf::get_or(options, "system.node", false))
-    return make_error(ec::invalid_configuration,
-                      "unable to spawn a remote source when spawning a node "
-                      "locally instead of connecting to one; please unset "
-                      "the option system.node");
-  auto accountant = accountant_type{};
-  if (auto a = self->system().registry().get(atom::accountant_v))
-    accountant = caf::actor_cast<accountant_type>(a);
-  auto src_result = make_source<Reader, Defaults, caf::detached>(
-    self, self->system(), args.inv, std::move(accountant), st.type_registry,
-    st.importer);
-  if (!src_result)
-    return src_result.error();
-  auto src = std::move(src_result->src);
-  auto name = std::move(src_result->name);
-  VAST_INFO(self, "spawned a", name, "source");
-  src->attach_functor([=](const caf::error& reason) {
-    if (!reason || reason == caf::exit_reason::user_shutdown)
-      VAST_INFO(name, "source shut down");
-    else
-      VAST_WARNING(name, "source shut down with error:", reason);
-  });
-  return src;
-}
+maybe_actor spawn_csv_source(node_actor* self, spawn_arguments& args);
+
+/// Tries to spawn a new SOURCE for the JSON format.
+/// @param self Points to the parent actor.
+/// @param args Configures the new actor.
+/// @returns a handle to the spawned actor on success, an error otherwise
+maybe_actor spawn_json_source(node_actor* self, spawn_arguments& args);
+
+/// Tries to spawn a new SOURCE for the PCAP format.
+/// @param self Points to the parent actor.
+/// @param args Configures the new actor.
+/// @returns a handle to the spawned actor on success, an error otherwise
+maybe_actor spawn_pcap_source(node_actor* self, spawn_arguments& args);
+
+/// Tries to spawn a new SOURCE for the Suricata format.
+/// @param self Points to the parent actor.
+/// @param args Configures the new actor.
+/// @returns a handle to the spawned actor on success, an error otherwise
+maybe_actor spawn_suricata_source(node_actor* self, spawn_arguments& args);
+
+/// Tries to spawn a new SOURCE for the Syslog format.
+/// @param self Points to the parent actor.
+/// @param args Configures the new actor.
+/// @returns a handle to the spawned actor on success, an error otherwise
+maybe_actor spawn_syslog_source(node_actor* self, spawn_arguments& args);
+
+/// Tries to spawn a new SOURCE for generating random data.
+/// @param self Points to the parent actor.
+/// @param args Configures the new actor.
+/// @returns a handle to the spawned actor on success, an error otherwise
+maybe_actor spawn_test_source(node_actor* self, spawn_arguments& args);
+
+/// Tries to spawn a new SOURCE for the Zeek format.
+/// @param self Points to the parent actor.
+/// @param args Configures the new actor.
+/// @returns a handle to the spawned actor on success, an error otherwise
+maybe_actor spawn_zeek_source(node_actor* self, spawn_arguments& args);
+>>>>>>> parent of 4d1a2d64... Merge pull request #964
 
 } // namespace vast::system
